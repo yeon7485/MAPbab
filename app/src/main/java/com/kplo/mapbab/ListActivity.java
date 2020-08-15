696a059b3dd8a6901c11fff024204f53c74cad5a
yeon7485 <wsx4379@naver.com>
2020-08-16 02:58:14 +0900

    글작성(제목,날짜,시간)

--- app/src/main/java/com/kplo/mapbab/ListActivity.java
+++ app/src/main/java/com/kplo/mapbab/ListActivity.java
@@ -16,17 +16,13 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        recyclerView = (RecyclerView) findViewById(R.id.list_recycler_view);
 
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
         recyclerView.setHasFixedSize(true);
 
-        // use a linear layout manager
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        // specify an adapter (see also next example)
      //   mAdapter = new MyAdapter(myDataset);
         recyclerView.setAdapter(mAdapter);
     }
