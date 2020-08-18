9557f73646a26c593458f81793d1cb86d52546ec
sb8956 <65844764+sb8956@users.noreply.github.com>
2020-08-18 21:50:49 +0900

    google naver

--- app/src/main/java/com/kplo/mapbab/MapsNaverActivity.java
+++ app/src/main/java/com/kplo/mapbab/MapsNaverActivity.java
@@ -10,13 +10,14 @@ import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.LocationOverlay;
 import com.naver.maps.map.overlay.Marker;
-
-
 
 public class MapsNaverActivity extends Activity implements OnMapReadyCallback {
 
     private MapView mapView;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,16 +25,22 @@ public class MapsNaverActivity extends Activity implements OnMapReadyCallback {
 
         mapView = findViewById(R.id.map_view);
         mapView.getMapAsync(this);
+
+
     }
 
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
+        LocationOverlay locationOverlay = naverMap.getLocationOverlay();
+        locationOverlay.setVisible(true);
+
+        locationOverlay.setPosition(new LatLng(37.221890, 127.186652));
+
+
         Marker marker = new Marker();
         marker.setPosition(new LatLng(37.221890, 127.186652));
         marker.setMap(naverMap);
-
-
+        
     }
-
-
 }
+
