199b6cd351c2e204e7d02d08f4864e7f24a3f2a0
sb8956 <65844764+sb8956@users.noreply.github.com>
2020-08-18 22:38:00 +0900

    now position

--- app/src/main/java/com/kplo/mapbab/MapsNaverActivity.java
+++ app/src/main/java/com/kplo/mapbab/MapsNaverActivity.java
@@ -7,15 +7,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.overlay.LocationOverlay;
 import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.util.FusedLocationSource;
 
 public class MapsNaverActivity extends Activity implements OnMapReadyCallback {
 
     private MapView mapView;
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FusedLocationSource locationSource;
+    private NaverMap naverMap;
 
 
     @Override
@@ -26,21 +32,36 @@ public class MapsNaverActivity extends Activity implements OnMapReadyCallback {
         mapView = findViewById(R.id.map_view);
         mapView.getMapAsync(this);
 
-
+        locationSource =
+                new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
     }
 
     @Override
-    public void onMapReady(@NonNull NaverMap naverMap) {
-        LocationOverlay locationOverlay = naverMap.getLocationOverlay();
-        locationOverlay.setVisible(true);
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,  @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(
+                requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated()) { // 권한 거부됨
+                naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(
+                requestCode, permissions, grantResults);
+    }
 
-        locationOverlay.setPosition(new LatLng(37.221890, 127.186652));
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+        naverMap.setLocationSource(locationSource);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
 
 
         Marker marker = new Marker();
         marker.setPosition(new LatLng(37.221890, 127.186652));
         marker.setMap(naverMap);
-        
+
+
     }
 }
-
